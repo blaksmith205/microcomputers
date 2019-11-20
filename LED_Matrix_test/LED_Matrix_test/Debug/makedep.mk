5d943da23cc617ce0c9b7eba6a512344d4ebfd38
William Blanc <blaksmith57@gmail.com>
2019-11-20 13:02:14 -0500

    Improves consistency of LED matrix operation

--- LED_Matrix_test/LED_Matrix_test/Debug/makedep.mk
+++ LED_Matrix_test/LED_Matrix_test/Debug/makedep.mk
@@ -2,7 +2,5 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-i2c_master.c
-
 main.c
 
--- LED_Matrix_test/LED_Matrix_test/main.c
+++ LED_Matrix_test/LED_Matrix_test/main.c
@@ -25,55 +25,34 @@
 
 #define SET_BIT(port, bit)	port |= (1<<bit);
 #define CLR_BIT(port, bit)	port &= ~(1<<bit);
+#define LOW 0
+#define HIGH 1
 
-#define SPI_DDR DDRB	// Set SPI DDR register as DDRB
 #define DIN_PIN 3	// Set MOSI (DIN) to bit 3 of PORTB (pin 11)
 #define SCK_PIN	5	// Set Clock bit as bit 5 of PORTB (pin 13)
 #define CS_PIN	2	// Set CS bit as bit 2 of PORTB (pin 10)
 
-uint8_t spi_write(char data)
-{
-	// Start transmission
-	SPDR = data;
-	// Wait for transmission to complete. SPIF = spi flag (bit 7)
-	while(!(SPSR & (1 << SPIF)));
-	return SPDR;
-}
+uint16_t row0 = 0x0800;
+uint16_t row1 = 0x0700;
+uint16_t row2 = 0x0600;
+uint16_t row3 = 0x0500;
+uint16_t row4 = 0x0400;
+uint16_t row5 = 0x0300;
+uint16_t row6 = 0x0200;
+uint16_t row7 = 0x0100;
 
-void spi_write_int(int data){
-	CLR_BIT(PORTB, CS_PIN); // Select the matrix
-	spi_write(data >> 8); // Write higher bits
-	spi_write(data & 0xFF); // Write lower bits
-	SET_BIT(PORTB, CS_PIN); // De-select the matrix
-}
-
-void spi_init(uint8_t mosi_bit, uint8_t clk_bit, uint8_t cs_bit)
-{
-	// Set MOSI, SCK output, and CS as output, all others input
-	SPI_DDR = (1 << mosi_bit) | (1 << clk_bit) | (1 << cs_bit);
-	// Enable SPI, Master, set clock to high when idle, set clock rate F_CPU/16 or 1MHz, and send MSB first (DORD 0 by default)
-	SPCR = (1<<SPIF) | (1 << SPE) | (1 << MSTR) | (1 << SPR0);
-	_delay_ms(10); // Small delay for consistency
-	// Get Matrix out of shutdown mode by sending 0xXCX1 from datasheet
-	spi_write_int(0x0C01);
-	_delay_ms(10);
-	// Set scan limit to all 8 digits (rows) by sending 0xXBX7 from datasheet
-	spi_write_int(0x0B07);
-	_delay_ms(10);
-	// Clear all rows
-	for(uint16_t data = 0x0100; data <= 0x0800; data+=0x0100){
-		spi_write_int(data);
-		_delay_ms(10);
-	}
-}
+void spi_init(volatile uint8_t *DDR, uint8_t MOSI, uint8_t CLK, uint8_t CS);
+void spi_transfer(uint16_t data);
+void setIntensity(uint8_t intensity);
 
 int main(void)
 {
 	SET_BIT(PORTB, CS_PIN); // Don't select matrix until transmission
-	spi_init(DIN_PIN, SCK_PIN, CS_PIN);	// Initialize serial output pins
+	spi_init(&DDRB, DIN_PIN, SCK_PIN, CS_PIN);	// Initialize serial output pins on PORTB
+	setIntensity(15);
 	
-	for (uint16_t data = 0x0100; data <= 0x08FF; data+= ){
-		spi_write_int(data);
+	for (uint16_t data = 0x0100; data <= 0x08FF; data++){
+		spi_transfer(data);
 		_delay_ms(20);
 	}
 
@@ -81,5 +60,39 @@ int main(void)
 	SET_BIT(PORTB, 0); 
 	while(1);
 	return 0;
+}
+
+void spi_init(volatile uint8_t *DDR, uint8_t MOSI, uint8_t CLK, uint8_t CS)
+{
+	*DDR = (1<<MOSI)|(1<<CLK)|(1<<CS);		//MOSI and SCK are output, CS to select matrix
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);	//enable SPI as master
+	
+	spi_transfer(0x0900);			//disable decoding for all segments
+	spi_transfer(0x0B07);			//scan 8 8-segments
+	
+	// Clear all digits
+	for(uint16_t data = 0x0100; data <=0x0800; data += 0x0100){
+		spi_transfer(data);
+	}
+	
+	spi_transfer(0x0C01);			//turn on the matrix
+}
+
+void spi_transfer(uint16_t data)
+{
+	CLR_BIT(PORTB, CS_PIN);		// Select matrix
+	SPDR = data >> 8;			//start CMD transmission
+	while(!(SPSR & (1<<SPIF)));	//wait transfer finish
+	SPDR = data & 0xFF;			//start DATA transmission
+	while(!(SPSR & (1<<SPIF)));	//wait transfer finish
+	SET_BIT(PORTB, CS_PIN)		//terminate the packet by
+	_delay_ms(1);				// Wait a little bit
+}
+
+void setIntensity(uint8_t intensity)
+{
+	if (intensity < 0 || intensity > 15)
+	return;
+	spi_transfer(0x0A00 | intensity);
 }
 
